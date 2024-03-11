6d1cfdfd6bbee5a152e93d7036719901bb901c06
Daniil Egorov <brooks_danik@ro.ru>
2024-03-11 22:12:29 +0300

    Merge pull request #1 from Egoroff33/noob-code
    delete idea folder

--- src/MatrixDemo.java
+++ src/MatrixDemo.java
@@ -1,6 +1,6 @@
 public class MatrixDemo {
     public static void main(String[] args) {
-        int [][] matrix = {
+        int[][] matrix = {
                 {0 * 0, 1 * 0, 2 * 0, 3 * 0},
                 {0 * 1, 1 * 1, 2 * 1, 3 * 1},
                 {0 * 2, 1 * 2, 2 * 2, 3 * 2},
@@ -8,9 +8,10 @@ public class MatrixDemo {
         };
         int i, j;
         for (i = 0; i < matrix.length; i++) {
-            for (j = 0; j < matrix.length; j++)
+            for (j = 0; j < matrix.length; j++) {
                 System.out.print(matrix[i][j] + " ");
-            System.out.println();
+                System.out.println();
+            }
         }
     }
 }
