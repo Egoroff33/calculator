55d7134ce95b9eb9c5b5665ac5a2eff368fc2a39
Daniil Egorov <brooks_danik@ro.ru>
2024-03-18 20:03:18 +0300

    Merge pull request #1 from Egoroff33/noob-code
    delete idea folder

--- src/BoolLogic.java
+++ src/BoolLogic.java
@@ -12,5 +12,23 @@ public class BoolLogic {
         System.out.println("e = " + e);
         System.out.println("f = " + f);
         System.out.println("g = " + g);
+
+        int i = 1, x = 1, z = 0;
+        if (i == 1 & x++ < 100) {
+            z = 100;
+            System.out.println("x = " + x);
+            System.out.println("z = " + z);
+        } else {
+            System.out.println("Ошибка, условие не выполняется");
+        }
+
+        int denom = 2, num = 200;
+        if (denom != 0 && num / denom > 10) {
+            System.out.println("denom = " + denom);
+            System.out.println("num = " + num);
+        } else {
+            System.out.println("Ошибка, условие не выполняется");
+        }
+
     }
 }
